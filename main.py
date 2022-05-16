a4ea69fca9b178943830dc6cec1293dde42d95a7
Ador464 <61640265+Ador464@users.noreply.github.com>
2022-05-16 11:20:33 -0300

    Só um update
    Correções e lógica :)

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 from time import time
 
 class Processo:
-  def __init__(self, base):
-    self.base = base
-    self.atual = 0
-
-def runProcesso(processo):
-  if processo.atual < 100000:
-    inicio = time()
-    while processo.atual < 100000:
-      processo.base * processo.base
-      processo.atual += 1
-      if time() >= inicio + quantum:
-        print('foi')
-        break
-  else:
-    fila.remove(processo)
-    print(f'Processo {processo.base} terminou!')
+    def __init__(self, base):
+        self.base = base
+        self.atual = 100000
 
 quantum = 1
-
+preempções = 0
 processo1 = Processo(2)
 processo2 = Processo(3)
 processo3 = Processo(5)
 
 fila = [processo1, processo2, processo3]
+começo = time()
 
 while fila:
-  runProcesso(processo1)
-  runProcesso(processo2)
-  runProcesso(processo3)
+    inicio = time()
+    while fila[0].atual > 0 and time() < inicio + (quantum/1000):
+        fila[0].base * fila[0].base
+        fila[0].atual -= 1
+    if fila[0].atual == 0:
+        print(f'o processo {fila[0].base} terminou de executar após { inicio -começo :.4f} segundos!')
+        fila.pop(0)
+    else:
+        fila.append(fila.pop(0))
+    preempções += 1
+print(f'tudo pronto, houveram {preempções} preempções no total')
