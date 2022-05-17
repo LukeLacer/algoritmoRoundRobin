e823f8c2b743308c7c64f020e1cbffecdb642e96
LukeLacer <lucas_lacer@hotmail.com>
2022-05-16 21:40:23 -0300

    save data in file

--- main.py
+++ main.py
@@ -5,33 +5,44 @@ class Processo:
         self.base = base
         self.atual = 0
         self.preempcoes = 0
+        self.tempo_execucao = 0
 
-quantum = 0.001 # em segundos
-processo1 = Processo(2)
-processo2 = Processo(3)
-processo3 = Processo(5)
+def runProcessos(i, quantum):
+    processo1 = Processo(2)
+    processo2 = Processo(3)
+    processo3 = Processo(5)
 
-fila = [processo1, processo2, processo3]
-comeco = time()
+    fila = [processo1, processo2, processo3]
+    comeco = time()
 
-while fila:
-    inicio = time()
-    while fila[0].atual < 100000 and time() < inicio + (quantum):
-        fila[0].base * fila[0].base
-        fila[0].atual += 1
-    if fila[0].atual == 100000:
-        fila[0].preempcoes += 1
-        print(f'\n================== Processo {fila[0].base} ==================')
-        print(f'Tempo de execução:   {inicio - comeco :.3f} s')
-        print(f'Total de preempções: {fila[0].preempcoes} preempções')
-        print(f'================================================\n')
-        fila.pop(0)
-    else:
-        fila[0].preempcoes += 1
-        fila.append(fila.pop(0))
+    while fila:
+        inicio = time()
+        while fila[0].atual < 100000 and time() < inicio + (quantum):
+            fila[0].base * fila[0].base
+            fila[0].atual += 1
+        if fila[0].atual == 100000:
+            fila[0].preempcoes += 1
+            print(f'\n================== Processo {fila[0].base} ==================')
+            fila[0].tempo_execucao = inicio - comeco
+            print(f'Tempo de execução:   {fila[0].tempo_execucao :.3f} s')
+            print(f'Total de preempções: {fila[0].preempcoes} preempções')
+            print(f'================================================\n')
+            fila.pop(0)
+        else:
+            fila[0].preempcoes += 1
+            fila.append(fila.pop(0))
 
+    with open("dados.csv","a") as arquivo:
+        arquivo.write(f'{i};{quantum};processo1;{processo1.tempo_execucao};{processo1.preempcoes}\n')
+        arquivo.write(f'{i};{quantum};processo2;{processo2.tempo_execucao};{processo2.preempcoes}\n')
+        arquivo.write(f'{i};{quantum};processo3;{processo3.tempo_execucao};{processo3.preempcoes}\n')
+    arquivo.close()
 
-total_preempçoes = processo1.preempcoes + processo2.preempcoes + processo3.preempcoes
-print(f'Total de preempções: {total_preempçoes}')
+    total_preempçoes = processo1.preempcoes + processo2.preempcoes + processo3.preempcoes
+    print(f'Total de preempções: {total_preempçoes}')
 
-
+for i in range(1,11):
+    runProcessos(i, 0.001)
+    runProcessos(i, 0.01)
+    runProcessos(i, 0.1)
+    runProcessos(i, 1)
